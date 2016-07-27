559e131985c1a291e87172a954c582478283261e
PRouzeau <pierre@rouzeau.net>
2016-07-28 01:47:07 +0200

    allow imglist in page list

--- edx/listpages.php
+++ edx/listpages.php
@@ -4,7 +4,7 @@
 	$pagelist = scandir($dir);
 	sort ($pagelist);
 	foreach ($pagelist as $page){
-		if (substr ($page, strlen ($page)-4) == ".txt" && $page!="hlp.txt" && $page!="aqlpreview.txt" && $page!="imglist.txt")
+		if (substr ($page, strlen ($page)-4) == ".txt" && $page!="hlp.txt" && $page!="aqlpreview.txt")
 			$data.= substr($page, 0, strlen ($page)-4)."\n"; 
 	}
 	echo $data;
